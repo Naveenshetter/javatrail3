70cdd2f0efd28c3f37f6d9d59294ec5d5646b3ea
Naveenshetter <124671593+Naveenshetter@users.noreply.github.com>
2023-02-26 20:45:05 +0530

    Create CompareArray.java

---
+++ CompareArray.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class CompareArray
+{
+  public static void main(String ar[])
+  {
+    Scanner sc=new Scanner();
+    int i,j,temp=0;
+    System.out.prinln("Enter number of elements in Array");
+    int N=sc.nextInt();
+    int A[]=new A[N];
+    int B[]=new B[N];
+    System.out.println("Enter elements of 1st Array");
+    for(i=0;i<N;i++)
+    {
+      A[i]=sc.nextInt();
+    }
+    for(i=0;i<N;i++)
+    {
+      B[i]=sc.nextInt();
+    }
+    //making Both Arrays ascending order
+    for(i=0;i<n-i;i++)
+    {
+      for(j=0;j<n-1;j++)
+      {
+        if(A[j]>A[j+1])
+        {
+          temp=A[j];
+          A[j]=A[j+1];
+          A[j+1]=temp;
+      }
+    
+    
+      
+    
